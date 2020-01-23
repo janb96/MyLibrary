53b77787f304278753655f838ee30e6d32532472
janb96 <janekb1996@gmail.com>
2020-01-24 00:29:36 +0100

    [NF] Add new routes GET,POST,DELETE(/books)

--- MyLibrary_Backend/App/routes/book.js
+++ MyLibrary_Backend/App/routes/book.js
@@ -1,12 +1,50 @@
 var express = require('express');
 var router = express.Router();
 let books = require('./../model/book.js');
-const Sequelize = require('sequelize');
-const axios = require('axios')
+const ISBN = require('isbn').ISBN;
 
 router.get('/', async function(req, res, next) {
   	let response = await books.findAll();
   	res.send(response);
 });
+
+router.post('/', async function(req, res, next) {
+
+	const bookName = req.body.bookName;
+	const bookAuthor = req.body.bookAuthor;
+	const bookISBN = ISBN.parse(req.body.bookISBN);
+
+	if(bookISBN == null) {
+		res.send("ISBN is incorrect");
+	} else if(bookISBN.isValid()) {
+		const book = {
+			bookName: bookName,
+			bookAuthor: bookAuthor,
+			bookISBN: bookISBN.asIsbn13()
+		};
+
+		books.create(book).then(
+			result => res.send(result)
+		);
+
+	} else {
+		res.send("ISBN is incorrect");
+	}
+
+});
+
+router.delete('/', async function(req, res, next) {
+	const bookID = req.body.bookID;
+
+	let response = await books.destroy({
+			where: {
+				bookID: bookID
+			}
+		}
+	);
+
+	res.send(response);
+});
+
 
 module.exports = router;
